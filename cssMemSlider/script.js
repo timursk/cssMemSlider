5e3a41ad9228ef7b9c0a9d43a28e4967e3e9f450
Timur Salikhov <timur.dev@mail.ru>
2021-10-17 07:50:44 +0300

    feat: finish slider js plus all animations (Sun, Oct 17, 2021 07:50:00 AM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -1,9 +1,11 @@
 const img = document.querySelectorAll('.slider-img'),
       dot = document.querySelectorAll('.dot'),
+      dotContainer = document.querySelectorAll('.dot-container'),
       str = document.querySelector('.controls-info');
+let index = 0;
+const text = ['Real BONK!er', 'Remember NO horny', 'BreaDog', 'We are leaving'];
 
-
-dot.forEach((item, idx) => {
+dotContainer.forEach((item, idx) => {
     item.addEventListener('click', () => { changeSlide(idx) })
 });
 
@@ -11,6 +13,16 @@ function changeSlide(idx) {
     removeClass(idx);
     dot[idx].classList.add('active');
     img[idx].classList.add('active');
+    if (idx > index) img[idx].classList.add('to-left');
+    else img[idx].classList.add('to-right');
+    str.textContent = text[idx];
+    str.classList.add('to-right');
+    index = idx;
+    setTimeout(() => {
+        img[idx].classList.remove('to-left');
+        img[idx].classList.remove('to-right');
+        str.classList.remove('to-right');
+    },500);
 }
 
 function removeClass(idx) {
